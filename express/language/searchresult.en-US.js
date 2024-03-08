ea5b1d5b1df067da4eb724141cd65191e231aec2
jimupark <qkrwlan@naver.com>
2024-03-08 22:55:00 +0900

    update noresult language

--- express/language/searchresult.en-US.js
+++ express/language/searchresult.en-US.js
@@ -2,6 +2,8 @@ const content = {
     search: 'Search', 
     reviewTitle: "Review Score",
     currency : 'JPY',
+    noResult : 'we could not found the treasures matching',
+    newSearch : 'How about new search?',
     
 }
 
--- express/language/searchresult.ja.js
+++ express/language/searchresult.ja.js
@@ -2,6 +2,8 @@ const content = {
     search: '検索', 
     reviewTitle: "レビュースコア",
     currency : '円',
+    noResult : '宝物に合致するものが見つかりませんでした',
+    newSearch : '新しい検索はいかがですか?',
 }
 
 module.exports=content
